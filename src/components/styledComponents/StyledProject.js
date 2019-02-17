8c6260d97b81f13034ae5bfdee34b2515e79398d
Kelly Stern <kmstern4@gmail.com>
2019-02-16 18:37:26 -0700

    styled components added

---
+++ src/components/styledComponents/StyledProject.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const StyledProject = styled.img`
+    background: black;
+    width: 90%;
+`;
+
+export default StyledProject;
