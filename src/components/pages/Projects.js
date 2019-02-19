6b4cfe47e9eb389ba0d8c9e7406afb5c684eccbf
Kelly Stern <kmstern4@gmail.com>
2019-02-18 17:11:48 -0700

    fixing commit/merging errors

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Row from '../grid/Row';
+import Column from '../grid/Column';
+import StyledProject from '../styledComponents/StyledProject';
 
 const Projects = () => {
     return <>
-        <p>Projects will go riiiiight here</p>
+        <Row>
+            <Column size='six'>
+                <StyledProject src='assets/mz.png' alt='Medieval Zombies' />
+            </Column>
+            <Column size='six'>
+                <StyledProject src='assets/ts.png' alt='Train Scheduler' />
+            </Column>
+        </Row>
     </>
 }
 
