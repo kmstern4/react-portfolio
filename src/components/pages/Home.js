5655565dbdabe8ec17a43fa1c148aa60b7e2c9ad
Kelly Stern <kmstern4@gmail.com>
2019-02-27 15:39:45 -0700

    added title styling on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Row from '../grid/Row';
 import Column from '../grid/Column';
 import StyledAbout from '../styledComponents/StyledAbout';
+import StyledTitle from '../styledComponents/StyledTitle';
 
 const Home = () => {
     return <>
@@ -10,11 +11,17 @@ const Home = () => {
                 <p>Image will go right here.</p>
             </Column>
             <Column size='six'>
+                <StyledTitle>
+                    About Me
+                </StyledTitle>
                 <StyledAbout>
-                    I am a front-end focused full stack web developer. I pride myself in making my projects look and function as clean as possible.
+                    I am a front-end focused full stack web developer. I pride myself in making my projects look and function as clean as possible. Currently, I am creating a YouTube tutorial series about Phaser.js, a library in which I am making a simple game. If you'd like to check out the game or any of my other projects, click the Projects link to the left.
                 </StyledAbout>
             </Column>
             <Column size='three'>
+                <StyledTitle>
+                    Technologies
+                </StyledTitle>
                 <p>Technology list will go right over here.</p>
             </Column>
         </Row>
---
+++ src/components/styledComponents/StyledTitle.js
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+const StyledTitle = styled.h2`
+    font-family: 'Pacifico', cursive;
+    font-size: 2vw;
+    color: #fc4a1a;
+`;
+
+export default StyledTitle;
