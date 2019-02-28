c3787cab07cc51cac3068af1306bf92bdc90e14e
Kelly Stern <kmstern4@gmail.com>
2019-02-27 17:32:20 -0700

    added github link to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { ContactList, ContactItem } from '../styledComponents/StyledContact';
 
 const Contact = () => {
     return <>
-        <ul>
-            <li>Email</li>
-            <li>GitHub</li>
-            <li>LinkedIn</li>
-        </ul>
+        <ContactList>
+            <ContactItem>Email</ContactItem>
+            <ContactItem><a href='https://github.com/kmstern4'>GitHub</a></ContactItem>
+            <ContactItem>LinkedIn</ContactItem>
+        </ContactList>
     </>
 }
 
---
+++ src/components/styledComponents/StyledContact.js
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+export const ContactList = styled.ul`
+    list-style-type: none;
+`;
+
+export const ContactItem = styled.li`
+    font-size: 1.3vw;
+`;
